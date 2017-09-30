0791653a7399e87949e40a0c12b3c814743709d7
kimkaeul <kaeul.kim@gmail.com>
2017-09-30 21:20:48 +0900

    github Test

---
+++ test.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+class test:
+    def search(self, path):
+        print path
+        filenames = os.listdir(path)
+        for filename in filenames:
+            fullFileName = os.path.join(path, filename)
+            print fullFileName
+
+   def search2(self, path):
+        for (path, dir, files) in os.walk(path):
+            for filename in files:
+                ext = os.path.splitext(filename)[-1]
+                if ext == ‘.py’:
+                    print(“%s/%s” % (path, filename))
+
+if __name__ == “__main__“:
+    test = test()
+    path = sys.argv[1]
+
+   test.search2(path)
+    test.search(path)
